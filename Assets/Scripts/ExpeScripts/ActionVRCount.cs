02b3cbb53fd7c479376bf3f15cf041da3b98a231
unknown <jean-baptiste.bordier@irisa.fr>
2021-11-03 15:15:14 +0100

    resolved input counting problem, should be working, needs test

--- Assets/Scripts/ExpeScripts/ActionVRCount.cs
+++ Assets/Scripts/ExpeScripts/ActionVRCount.cs
@@ -7,65 +7,18 @@ public class ActionVRCount : MonoBehaviour
 {
 
     public int numberOfAction;
-    private string previousButton = "";
-
-
-
-
-    // Update is called once per frame
+      // Update is called once per frame
     void Update()
     {
-        if( VRInput.primaryControllerValues.gripButtonPressed && !previousButton.Contains("gripP"))
-        {
-            previousButton = "gripP";
-            numberOfAction++;
-        }
-        if (VRInput.primaryControllerValues.triggerButtonPressed && !previousButton.Contains("triggerP"))
-        {
-            previousButton = "triggerP";
-            numberOfAction++;
-        }
-        if (VRInput.primaryControllerValues.primary2DAxisClickState && !previousButton.Contains("AxisP"))
-        {
-            previousButton = "AxisP";
-            numberOfAction++;
-        }
-        if (VRInput.primaryControllerValues.primaryButtonState && !previousButton.Contains("primaryBP"))
-        {
-            previousButton = "primaryBP";
-            numberOfAction++;
-        }
-        if (VRInput.primaryControllerValues.secondaryButtonState && !previousButton.Contains("secondaryBP"))
-        {
-            previousButton = "secondaryBP";
-            numberOfAction++;
-        }
-        if (VRInput.secondaryControllerValues.gripButtonPressed && !previousButton.Contains("gripS"))
-        {
-            previousButton = "gripS";
-            numberOfAction++;
-        }
-        if (VRInput.secondaryControllerValues.triggerButtonPressed && !previousButton.Contains("triggerS"))
-        {
-            previousButton = "triggerS";
-            numberOfAction++;
-        }
-        if (VRInput.secondaryControllerValues.primary2DAxisClickState && !previousButton.Contains("AxisS"))
-        {
-            previousButton = "AxisS";
-            numberOfAction++;
-        }
-        if (VRInput.secondaryControllerValues.primaryButtonState && !previousButton.Contains("primaryBS"))
-        {
-            previousButton = "primaryBS";
-            numberOfAction++;
-        }
-        if (VRInput.secondaryControllerValues.secondaryButtonState && !previousButton.Contains("secondaryBS"))
-        {
-            previousButton = "secondaryBS";
-            numberOfAction++;
-        }
-
-        //VRINPUT GET INPUT STATE ! record which one was press and check if it is not the same : if not : ++
+        VRInput.ButtonEvent(VRInput.primaryController, CommonUsages.grip, onRelease: () => { numberOfAction++; });
+        VRInput.ButtonEvent(VRInput.primaryController, CommonUsages.trigger, onRelease: () => { numberOfAction++; });
+        VRInput.ButtonEvent(VRInput.primaryController, CommonUsages.primaryButton, onRelease: () => { numberOfAction++; });
+        VRInput.ButtonEvent(VRInput.primaryController, CommonUsages.primary2DAxisClick, onRelease: () => { numberOfAction++; });
+        VRInput.ButtonEvent(VRInput.primaryController, CommonUsages.secondaryButton, onRelease: () => { numberOfAction++; });
+        VRInput.ButtonEvent(VRInput.secondaryController, CommonUsages.grip, onRelease: () => { numberOfAction++; });
+        VRInput.ButtonEvent(VRInput.secondaryController, CommonUsages.trigger, onRelease: () => { numberOfAction++; });
+        VRInput.ButtonEvent(VRInput.secondaryController, CommonUsages.primaryButton, onRelease: () => { numberOfAction++; });
+        VRInput.ButtonEvent(VRInput.secondaryController, CommonUsages.primary2DAxisClick, onRelease: () => { numberOfAction++; });
+        VRInput.ButtonEvent(VRInput.secondaryController, CommonUsages.secondaryButton, onRelease: () => { numberOfAction++; });
     }
 }
